0ffd579e9209af06edd76e9bfc7ea6df65428055
manaskhanger <khangermanas@gmail.com>
2026-04-08 12:56:14 +0000

    Solved Search in Rotated Sorted Array

--- solutions/2026-04-08.py
+++ solutions/2026-04-08.py
@@ -1,20 +1,3 @@
-# 3Sum (Medium)
+# Search in Rotated Sorted Array (Medium)
 
-def three_sum(nums):
-    nums.sort()
-    res = []
-    for i in range(len(nums)):
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        l, r = i+1, len(nums)-1
-        while l < r:
-            s = nums[i] + nums[l] + nums[r]
-            if s == 0:
-                res.append([nums[i], nums[l], nums[r]])
-                l += 1
-                r -= 1
-            elif s < 0:
-                l += 1
-            else:
-                r -= 1
-    return res
+print('Solution coming soon')
