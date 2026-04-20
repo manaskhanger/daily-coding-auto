4e99c409f925bea6f53950e541928264a0cc078c
manaskhanger <khangermanas@gmail.com>
2026-04-20 13:03:25 +0000

    Solved 3Sum

--- solutions/2026-04-20.py
+++ solutions/2026-04-20.py
@@ -1,3 +1,20 @@
-# Search in Rotated Sorted Array (Medium)
+# 3Sum (Medium)
 
-print('Solution coming soon')
+def three_sum(nums):
+    nums.sort()
+    res = []
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        l, r = i+1, len(nums)-1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if s == 0:
+                res.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+            elif s < 0:
+                l += 1
+            else:
+                r -= 1
+    return res
