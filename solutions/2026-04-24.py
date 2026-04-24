7d4716a1bd23b2b66c85cdd0546a792960279bbf
manaskhanger <khangermanas@gmail.com>
2026-04-24 08:10:17 +0000

    Worked on 3Sum  problem

--- solutions/2026-04-24.py
+++ solutions/2026-04-24.py
@@ -1,11 +1,20 @@
-# Longest Substring Without Repeating Characters (Medium)
+# 3Sum (Medium)
 
-def longest_substring(s):
-    seen = {}
-    l = res = 0
-    for r in range(len(s)):
-        if s[r] in seen:
-            l = max(l, seen[s[r]] + 1)
-        seen[s[r]] = r
-        res = max(res, r - l + 1)
+def three_sum(nums):
+    nums.sort()
+    res = []
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        l, r = i+1, len(nums)-1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if s == 0:
+                res.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+            elif s < 0:
+                l += 1
+            else:
+                r -= 1
     return res
