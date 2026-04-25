7549a357277ae839ceb2a01b2726ae69488cc71e
manaskhanger <khangermanas@gmail.com>
2026-04-25 12:41:50 +0000

    Practiced Longest Substring Without Repeating Characters

--- solutions/2026-04-25.py
+++ solutions/2026-04-25.py
@@ -1,20 +1,11 @@
-# 3Sum (Medium)
+# Longest Substring Without Repeating Characters (Medium)
 
-def three_sum(nums):
-    nums.sort()
-    res = []
-    for i in range(len(nums)):
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        l, r = i+1, len(nums)-1
-        while l < r:
-            s = nums[i] + nums[l] + nums[r]
-            if s == 0:
-                res.append([nums[i], nums[l], nums[r]])
-                l += 1
-                r -= 1
-            elif s < 0:
-                l += 1
-            else:
-                r -= 1
+def longest_substring(s):
+    seen = {}
+    l = res = 0
+    for r in range(len(s)):
+        if s[r] in seen:
+            l = max(l, seen[s[r]] + 1)
+        seen[s[r]] = r
+        res = max(res, r - l + 1)
     return res
