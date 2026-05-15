2b5703f0c6059183ad4290e0bfec0591b6009b5d
manaskhanger <khangermanas@gmail.com>
2026-05-15 08:51:59 +0000

    DSA practice: Trapping Rain Water

--- solutions/2026-05-15.py
+++ solutions/2026-05-15.py
@@ -1,8 +1,3 @@
-# Median of Two Sorted Arrays (Hard)
+# Trapping Rain Water (Hard)
 
-def find_median(a, b):
-    nums = sorted(a + b)
-    n = len(nums)
-    if n % 2:
-        return nums[n//2]
-    return (nums[n//2 - 1] + nums[n//2]) / 2
+print('Solution coming soon')
